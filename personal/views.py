ec719ed81557016d4bddaaf4d06e0aaffb58cf15
avishekh <avishekh.bharati@gmail.com>
2016-08-11 08:12:11 +0545

    database migrationa & create opertaion of blog > posts

--- personal/views.py
+++ personal/views.py
@@ -4,4 +4,4 @@ def index(request):
 	return render(request, 'personal/home.html')
 
 def contact(request):
-	return render(request, 'personal/basic.html', {'content':['email me at ::', 'avi@yahoo.com']}
+	return render(request, 'personal/basic.html', {'content':['email me at ::', 'avi@yahoo.com']})
